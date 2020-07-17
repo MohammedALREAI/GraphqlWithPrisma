d12ed9e43022e8567dee78ead7dd0613f8135cab
unknown <mhamad.aa1997.aa@gmail.com>
2020-07-17 10:10:03 +0300

    add the graphql file

--- src/prisma.js
+++ src/prisma.js
@@ -2,7 +2,7 @@ import { Prisma } from "prisma-binding";
 
 const prisma = new Prisma({
   typeDefs: "src/generated/prisma.graphql",
-     endpoint: "https://eu1.prisma.sh/mhamadaa1997/app/dev",
+     endpoint: "https://eu1.prisma.sh/mhamadaa1997/mohammed/dev",
   secret: "mysecretKey",
 
 });
--- src/resolvers/Mutation.js
+++ src/resolvers/Mutation.js
@@ -1,4 +1,54 @@
 export default (Mutation = {
+async createpublisher(parent,args,ctx,info){},
+async updatepublisher(parent,args,ctx,info){},
+async deletepublisher(parent,args,ctx,info){},
+async createclassRooms(parent,args,ctx,info){},
+async updateclassRooms(parent,args,ctx,info){},
+async deleteclassRooms(parent,args,ctx,info){},
+async createCatogory(parent,args,ctx,info){},
+async updateCatogory(parent,args,ctx,info){},
+async deleteCatogory(parent,args,ctx,info){},
+async createPubliserYear(parent,args,ctx,info){},
+async updatePubliserYear(parent,args,ctx,info){},
+async deletePubliserYear(parent,args,ctx,info){},
+async createCatogory(parent,args,ctx,info){},
+async updateCatogory(parent,args,ctx,info){},
+async deleteCatogory(parent,args,ctx,info){},
+async createpubliserYears(parent,args,ctx,info){},
+async updatepubliserYears(parent,args,ctx,info){},
+async deletepubliserYears(parent,args,ctx,info){},
+async createAuthor(parent,args,ctx,info){},
+async updateAuthor(parent,args,ctx,info){},
+async deleteAuthor(parent,args,ctx,info){},
+async createVedio(parent,args,ctx,info){},
+async updateVedio(parent,args,ctx,info){},
+async deleteVedio(parent,args,ctx,info){},
+async createCatogory(parent,args,ctx,info){},
+async updateCatogory(parent,args,ctx,info){},
+async deleteCatogory(parent,args,ctx,info){},
+async createBook(parent,args,ctx,info){},
+async updateBook(parent,args,ctx,info){},
+async createSubject(parent,args,ctx,info){},
+async updateSubject(parent,args,ctx,info){},
+async deleteSubject(parent,args,ctx,info){},
+async createBookTest(parent,args,ctx,info){},
+async updateBookTest(parent,args,ctx,info){},
+async deleteBookTest(parent,args,ctx,info){},
+async createQuestions(parent,args,ctx,info){},
+async updateQuestions(parent,args,ctx,info){},
+async deleteQuestions(parent,args,ctx,info){},
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 });
